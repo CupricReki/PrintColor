d4276998883983f492a362e241e830eab571269f
sogden <sogden@edgepower.com>
2017-11-27 03:29:02 -0700

    g

--- hue_update.py
+++ hue_update.py
@@ -63,7 +63,7 @@ def set_led(status):
     bed_target = status[1]
     print status
 
-    if bed_target != 0 or bed_target != bed_start:
+    if bed_target > 0 or bed_target != bed_start:
         temperature_percent = int(round(100*((bed_actual - bed_start) / (bed_target - bed_start))))
     else:
         temperature_percent = 0
