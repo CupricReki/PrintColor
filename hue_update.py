5ffee1f00b766265d4fcfc43ba30dda6438bbac5
sogden <sogden@edgepower.com>
2017-11-27 01:25:38 -0700

    Just playing

--- hue_update.py
+++ hue_update.py
@@ -14,14 +14,14 @@ import requests
 
 #Octoprint:
 # Use api key
-http://docs.octoprint.org/en/master/api/
+# http://docs.octoprint.org/en/master/api/
 
 
 # Dependant code libraries:
 
 # phue
 # Full featured Python library to control the Philips Hue lighting system.
-https://github.com/studioimaginaire/phue
+# https://github.com/studioimaginaire/phue
 
 # color converter
 
@@ -32,20 +32,12 @@ https://github.com/studioimaginaire/phue
 # debug = 1 -
 
 octoprint_api = '56D0FF611C184738B2CAE37CE1F7446F'
-octoprint_ip = '10.10.0.110'
+octoprint_ip = 'printerpi.lan'
 
 
-def get_octoprint_temp():
+def octoprint():
+    r = requests.get('http://printerpi.lan/api/printer?history=true&limit=2', headers = {'X-Api-Key': '56D0FF611C184738B2CAE37CE1F7446F'})
     # Get target and current temperature
 
 
-def get_octoprint_status():
-    # Get print status
-
-def update_huelight(color, brightness):
-    if brightness != '':
-        brightness = 255
-
-    # Takes in the desired color (in RGB) and translates it into the correct x,y colord
-
-while true:
+octoprint()
