cf7f8240ebad3dc9574deba17d7fa7625bee0db5
sogden <sogden@edgepower.com>
2017-11-27 02:40:29 -0700

    g

--- hue_update.py
+++ hue_update.py
@@ -61,10 +61,8 @@ def set_led(status):
     print status
     bed_actual = status[0]
     bed_target = status[1]
-    print (bed_actual - bed_start)
-    print ((bed_target - bed_start))
     temperature_percent = (bed_actual - bed_start) / (bed_target - bed_start)
-    print int(round(temperature_percent))
+    print temperature_percent
     return
 if __name__ == '__main__':
     # Create NeoPixel object with appropriate configuration.
