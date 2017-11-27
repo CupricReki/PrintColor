09ed9793323cfa868d7c3d0e370c901d61e287c6
sogden <sogden@edgepower.com>
2017-11-27 01:55:23 -0700

    Added ability to query for temperature data. Imported libraries for LED controlxcxvzcv.

--- hue_update.py
+++ hue_update.py
@@ -43,7 +43,7 @@ LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
 LED_BRIGHTNESS = 10     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0
-LED_STRIP      = ws.SK6812_STRIP_RGBW
+#LED_STRIP      = ws.SK6812_STRIP_RGBW
 
 
 # Initiate variables
