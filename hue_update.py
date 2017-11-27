6d97d44f13868fd62b778d9d6a7d57dc25e450da
sogden <sogden@edgepower.com>
2017-11-27 03:33:21 -0700

    g

--- hue_update.py
+++ hue_update.py
@@ -81,6 +81,7 @@ def set_led(status):
 
 
 if __name__ == '__main__':
+    bed_target_last = 0
     # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL,
                               LED_STRIP)
