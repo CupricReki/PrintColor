c00e1666ed93de68bcbe2e12da0cdb32a730ae83
sogden <sogden@edgepower.com>
2017-11-27 02:39:14 -0700

    Added interpolation code

--- hue_update.py
+++ hue_update.py
@@ -62,6 +62,7 @@ def set_led(status):
     bed_actual = status[0]
     bed_target = status[1]
     print (bed_actual - bed_start)
+    print ((bed_target - bed_start))
     temperature_percent = (bed_actual - bed_start) / (bed_target - bed_start)
     print int(round(temperature_percent))
     return
